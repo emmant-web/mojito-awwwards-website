625532dc2448b03e8dd508a77dec65630c2cf15d
emmant-web <emmanueldtoribio@gmail.com>
2026-04-08 10:41:23 +0800

    finished navbar section - version 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { SplitText } from 'gsap/SplitText'
 
+import Navbar from './components/Navbar';
+import './index.css'
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-red-400 text-3xl'>Hello World!</h1>
-    </div>
+    <main>
+        <Navbar />
+    </main>
   )
 }
 
-export default App
+export default App;
