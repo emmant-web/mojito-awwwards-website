68f6a467e8af58727cc795de9d664306fb07b369
emmant-web <emmanueldtoribio@gmail.com>
2026-04-09 08:18:39 +0800

    finished about section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './index.css'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Cocktails />
+        <About />
     </main>
   )
 }
